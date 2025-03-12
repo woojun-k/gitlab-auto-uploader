d68eb65231d448578f3ef47742db2ef78e7a3b5c
woojun-k <kwoojun.eng@gmail.com>
2025-03-12 11:48:53 +0900

    fix: update to track new files

--- upload.sh
+++ upload.sh
@@ -21,32 +21,30 @@ find "$base_dir" -type d -name ".git" | while read -r git_dir; do
         git config --local user.email [GITLAB_EMAIL]
     fi
 
-    find "$repo_dir" -type f -name "*.java" | while read -r java_file; do
-        file_name=$(basename "$java_file")
+    git status --short | while read -r status file; do
+        if [[ "$status" == "M"* || "$status" == "A"* ]]; then
+            if git diff --quiet "$file"; then
+                continue
+            fi
 
-        if git diff --quiet "$java_file"; then
-            continue
+            commit_message="add: solution for $dir_name"
+
+            echo "Processing: $file"
+            git add "$file"
+            echo "Added: $file"
+
+            git commit -m "$commit_message"
+            echo "Committed: $file with message: $commit_message"
+
         fi
-
-        commit_message="add: solution for $dir_name"
-
-        echo "Processing: $file_name"
-        git add "$java_file"
-        echo "Added: $file_name"
-
-        git commit -m "$commit_message"
-        echo "Committed: $file_name with message: $commit_message"
-
-        if ! git push origin master; then
-            echo "$repo_dir" >> "$failed_file"
-            echo "Failed to push: $repo_dir"
-        else
-            echo "Pushed: $file_name to remote"
-        fi
-        echo "-------------------------------"
     done
-
-echo "Finished processing repository: $repo_dir"
+    if ! git push origin master; then
+        echo "$repo_dir" >> "$failed_file"
+        echo "Failed to push: $repo_dir"
+    else
+        echo "Pushed: $repo_dir to remote"
+    fi
+    echo "-------------------------------"
 
 done
 
